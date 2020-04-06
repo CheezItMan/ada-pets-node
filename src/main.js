bfc2f91fa53a2d1595436682be27839fe7fdb653
CheezItMan <chris@adadevelopersacademy.org>
2020-04-06 13:41:43 -0700

    small update to main for breed/species update

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const removePet = adaPets.removePet;
 const addPet = adaPets.addPet;
 
 const exit = () => {
-  setResult("Thank you for using the Ada Pets Adoption App!");
+  setResult('Thank you for using the Ada Pets Adoption App!');
   process.exit();
 }
 
@@ -22,7 +22,7 @@ let selectedPet = null;
 const selectPet = (_, args) => {
   const petId = parseInt(args.petId, 10);
   if (isNaN(petId)) {
-    setError("Please provide a number for petId got: ${args.petId}");
+    setError('Please provide a number for petId got: ${args.petId}');
   } else {
     selectedPet = petId;
     setResult(petId);
@@ -31,7 +31,7 @@ const selectPet = (_, args) => {
 
 // Helper to log errors in red.
 const logError = (message) => {
-  console.error(`\x1b[1;31m${message}\x1b[0m`);
+  console.error(`\x1b[1;31m${ message }\x1b[0m`);
 }
 
 const doAction = (action, resultCallback) => {
@@ -49,7 +49,7 @@ const doAction = (action, resultCallback) => {
     };
 
     setHandlers(resultHandler, errorHandler, () => {
-      logError("Command failed: Took more than three seconds to produce a result!");
+      logError('Command failed: Took more than three seconds to produce a result!');
       done();
     }, 3000);
     action(selectedPet, args);
@@ -59,38 +59,38 @@ const doAction = (action, resultCallback) => {
 // Register the options.
 vorpal.find('exit').remove();
 vorpal
-  .command("exit", "exits the program")
+  .command('exit', 'exits the program')
   .action(doAction(exit));
 
 vorpal
-  .command("list pets", "list the pets from the API")
+  .command('list pets', 'list the pets from the API')
   .action(doAction(listPets, pets => {
     pets.forEach(pet => {
-      console.log(`${pet.id}: ${pet.name}`);
+      console.log(`${ pet.id }: ${ pet.name }`);
     });
   }));
 
 vorpal
-  .command("select pet <petId>", "select the pet with <petId>")
+  .command('select pet <petId>', 'select the pet with <petId>')
   .action(doAction(selectPet));
 
 vorpal
-  .command("show details", "show the details for the selected pet")
+  .command('show details', 'show the details for the selected pet')
   .action(doAction(showDetails));
 
 vorpal
-  .command("remove pet", "remove the selected pet")
+  .command('remove pet', 'remove the selected pet')
   .action(doAction(removePet));
 
 vorpal
-  .command("add a pet <name>", "add a new pet")
-  .option("-a, --age <age>", "The pet's age in years")
-  .option("-b, --breed <breed>", "The pet's breed or species")
-  .option("-o, --owner <owner>", "The pet's owner")
-  .option("--about <about>", "About the pet")
+  .command('add a pet <name>', 'add a new pet')
+  .option('-a, --age <age>', 'The pet\'s age in years')
+  .option('-b, --species <species>', 'The pet\'s breed or species')
+  .option('-o, --owner <owner>', 'The pet\'s owner')
+  .option('--about <about>', 'About the pet')
   .action(doAction((_, args) => addPet(args)));
 
-vorpal.exec("help");
+vorpal.exec('help');
 vorpal
   .delimiter('What would you like to do?')
   .show();
